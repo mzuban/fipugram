484d3c180f4803660709a5ae35006d6d4645ad9f
Maja Zuban <mzuban@unipu.hr>
2023-07-24 01:38:39 +0200

    Vue_03_Samostalni zadatak_podatci o vlasniku posta i modificiranje pretrage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,42 @@
 <template>
   <div class="row">
     <div class="col-8">
-        ... Instagram story!
-      <InstagramCard v-for="card in filteredCards" :key="card.url" :info="card" />
+      ... Instagram story!
+      <div v-for="card in filteredCards" :key="card.url" class="card">
+        <div class="card-header">{{ card.title }}</div>
+        <div class="card-body">
+          <img class="card-img-top" :src="card.url" alt="neka slika..." />
+        </div>
+        <div class="card-footer text-muted">
+          {{ card.time }}
+        </div>
+        <div class="card-owner">
+          <p>Ime: {{ card.owner.ime }}</p>
+          <p>Prezime: {{ card.owner.prezime }}</p>
+        </div>
+      </div>
     </div>
     <div class="col-4">
-      Sidebar
-      {{ store.searchTerm }}
     </div>
   </div>
 </template>
+
 <script>
-// @ is an alias to /src
-import InstagramCard from '@/components/InstagramCard.vue'
 import store from "@/store"
-// ovaj dio je uputa za VueJS kako mora oblikovati InstagramCard komponentu
 
-let cards =[];
+let cards = [];
 
-cards =[
-  {description: "jedan", time: "an hour ago", url:"https://picsum.photos/id/1/400/400" },
-  {description: "dva", time: "1 day ago", url:"https://picsum.photos/id/2/400/400" },
-  {description: "tri", time: "2 days ago", url:"https://picsum.photos/id/3/400/400" },
-  {description: "četiri", time: "3 days ago", url:"https://picsum.photos/id/4/400/400" },
-
+cards = [
+  { title: "Naslov 1", time: "an hour ago", url: "https://picsum.photos/id/1/400/400", owner: { ime: "Ivo", prezime: "Ivić" } },
+  { title: "Naslov 2", time: "1 day ago", url: "https://picsum.photos/id/2/400/400", owner: { ime: "Marko", prezime: "Marković" } },
+  { title: "Naslov 3", time: "2 days ago", url: "https://picsum.photos/id/3/400/400", owner: { ime: "Ana", prezime: "Anić" } },
+  { title: "Naslov 4", time: "3 days ago", url: "https://picsum.photos/id/4/400/400", owner: { ime: "Petra", prezime: "Petrić" } },
 ];
-//...API -> sve kartice ->cards
 
 export default {
   name: 'home',
-  data: function() {
-    return{
+  data() {
+    return {
       cards: cards,
       store,
     };
@@ -38,20 +44,27 @@ export default {
   computed: {
     filteredCards() {
       //logika koja filtrira cards
-      let termin = this.store.searchTerm;
-      let newCards = [];
-
-      for (let card of this.cards) {
-        if (card.description.indexOf(termin) >= 0) {
-          newCards.push(card);
-        }
-        console.log(newCards);
-      }
-      return newCards;
+      let searchTerm = this.store.searchTerm.toLowerCase();
+      return this.cards.filter(
+        (card) =>
+          card.title.toLowerCase().indexOf(searchTerm) >= 0 ||
+          (card.owner &&
+            (card.owner.ime.toLowerCase().indexOf(searchTerm) >= 0 ||
+              card.owner.prezime.toLowerCase().indexOf(searchTerm) >= 0))
+      );
     }
   },
-  components: {
-    InstagramCard,
-  },
+  components: {},
 };
 </script>
+
+<style lang="scss">
+.card {
+  margin-bottom: 20px;
+}
+.card-owner {
+  margin-top: 10px;
+  border: 1px solid #ccc;
+  padding: 10px;
+}
+</style>
